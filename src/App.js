6a290ac9d427c4f0d290b33e3489c893f9d074f3
Khyrelle Oberes <khynhoroberes.gmail.com>
2025-05-15 05:26:35 +0800

    save

--- src/App.js
+++ src/App.js
@@ -3,27 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login.js';
 import Home from './pages/Home';
 import Signup from './pages/Signup/Signup.js';
-import About from './pages/About/About.js';
-import Contact from './pages/Contact/Contact.js';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Search from './components/Header/Search/Search';
 
 function App() {
   return (
     <Router>
-      <Header />
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Search" element={<Search />} />
       </Routes>
-
-      <Footer />
     </Router>
   );
 }
