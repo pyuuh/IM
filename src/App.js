8f43cd3203b383a05b70a6ca559dc1b6a1900fa2
eastineee <paskytine@gmail.com>
2025-05-16 13:19:30 +0800

    lezgo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login.js';
 import Home from './pages/Home';
 import Signup from './pages/Signup/Signup.js';
+import About from './pages/About/About.js';
+import Contact from './pages/Contact/Contact.js';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Search from './components/Header/Search/Search';
+
 
 function App() {
   return (
@@ -13,7 +19,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Search" element={<Search />} />
+
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
